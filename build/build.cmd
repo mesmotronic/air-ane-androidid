d864da60c38b267de270410ed23df172a5213ca6
Neil Rackett <neil.rackett@mesmotronic.com>
2014-10-17 18:13:59 +0100

    Added SWCs and JARs

---
+++ build/build.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+REM "C:\Program Files\Adobe\Adobe Flash Builder 4.7 (64 Bit)\sdks\4.6.0\bin\adt" ^
+
+adt ^
+ -package ^
+ -target ane ./AndroidID.ane extension.xml ^
+ -swc swc/android-id-android.swc ^
+ -platform Android-ARM -C android . ^
+ -platform Android-x86 -C android . ^
+ -platform default -C default . 
