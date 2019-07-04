e34a04d01841dd8cc3a2445aa5d4dc1d687d4366
Neil Rackett <neil.rackett@mesmotronic.com>
2019-07-04 11:05:45 +0100

    Added support for Android-ARM64

--- build/build.cmd
+++ build/build.cmd
@@ -1,9 +1,10 @@
 @echo off
 
-"C:\Program Files\Adobe\Adobe Flash Builder 4.7 (64 Bit)\sdks\4.6.0\bin\adt" ^
+call "C:\Program Files\Adobe\Adobe Flash Builder 4.7 (64 Bit)\sdks\4.16.1 AIR33\bin\adt" ^
  -package ^
  -target ane ./AndroidID.ane extension.xml ^
  -swc swc/android-id-android.swc ^
  -platform Android-ARM -C android . ^
+ -platform Android-ARM64 -C android . ^
  -platform Android-x86 -C android . ^
  -platform default -C default . 
